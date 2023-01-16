a6b8a6943ba82f1c1baa61285a1a519b9a0d459f
Pr0gger1 <kraus2202200491@gmail.com>
2023-01-16 13:05:58 +0300

    ide-toolbox have been removed

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
-
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
             <App/>
-        </DevSupport>
     </React.StrictMode>
 );
